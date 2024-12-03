82503cd217bfb74b555646bb3dab91469f899487
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-02 23:50:02 -0500

    add function repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+  let result = string
+  let i = 1
+  while (i < num) {
+    result += string;
+    i++;
+  }
+  return result;
 };
+
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
