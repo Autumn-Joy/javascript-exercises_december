b6a48fe3c31558730a9f246222226b4335188743
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-04 00:36:48 -0500

    complete ex. 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+  const filteredArray = array.filter(items => !elements.includes(items));
+  return filteredArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
