86073d654734c6f92d3c3307288f231722901d71
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-04 02:30:46 -0500

    complete ex.6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let output;
+  if (year%100 == 0 && year%400 !== 0) {
+    output = false;
+  } else if (year%400 == 0) {
+    output = true;
+  } else if (year%4 == 0) {
+    output = true;
+  } else {
+    output = false;
+  }
+  return output;
 };
 
 // Do not edit below this line
