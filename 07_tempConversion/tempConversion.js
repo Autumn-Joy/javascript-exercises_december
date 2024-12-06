5c421ca8d3db9d6e53fa0781c0c6335df01cefa6
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-06 12:42:20 -0500

    complete ex. 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+let fahrenheit;
+let celcius;
+
+const convertToCelsius = function(fahrenheit) {
+//T(°F) = T(°C) × 9/5 + 32
+//°C = (°F - 32) ÷ (9/5)
+celcius = (fahrenheit - 32) / (9/5);
+celcius = Math.round(celcius * 10) / 10;
+return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  fahrenheit = celcius * (9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10
+  return fahrenheit;
 };
 
 // Do not edit below this line
