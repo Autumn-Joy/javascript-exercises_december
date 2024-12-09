b50005ea7d0fb2efbb29a03a43ab8808ee4c0843
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-08 21:05:54 -0500

    calculator--build add and subratract functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,26 @@
-const add = function() {
-	
+const add = function(...addedItems) {
+  let total = 0;
+  for (let item of addedItems) {
+    total += item;
+  }
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...subtractedItems) {
+  let total = subtractedItems[0];
+  subtractedItems.shift();
+  for (let item of subtractedItems) {
+    total -= item;
+  }
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(...addedItems) {
+  let total = 0;
+  for (let item of addedItems) {
+    total += item;
+  }
+  return total;
 };
 
 const multiply = function() {
@@ -15,11 +28,11 @@ const multiply = function() {
 };
 
 const power = function() {
-	
+
 };
 
 const factorial = function() {
-	
+
 };
 
 // Do not edit below this line
