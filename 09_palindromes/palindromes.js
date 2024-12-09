e7617cceae1b0ae5ae6c34c098df250be261bcbc
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-09 01:02:46 -0500

    build full ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,10 @@
 const palindromes = function (string) {
   //make an array of the word:
-  let wordArray = string.toLowerCase.split("")
+  let wordArray = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+  wordArray = wordArray.split('');
 
   //form a reversed copy of the array:
   let reversedWordArray = wordArray.toReversed();
-
     //simple conditional checks for palindrome-ness:
   if (wordArray.join('') === reversedWordArray.join('')) {
     return true;
