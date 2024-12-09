a546b4f6b11ef4d0f5204b633d3b2484eeeea586
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-09 15:50:26 -0500

    build ex.10 potential starting point

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(InputNumber) {
+  let FibonacciArray = [];
 
+  let OriginalNumber = InputNumber;
+  let firstNumber = (OriginalNumber -1);
+  let secondNumber = (OriginalNumber -2);
+  let iteration = InputNumber;
+  for (iteration; iteration <= 100; iteration++) {
+    // if (firstNumber <= 0) {
+    //   firstNumber = 0;
+    // };
+    // if (secondNumber <= 0) {
+    //   secondNumber = 0;
+    // ;}
+    if (inputNumber == 1) {
+      firstNumber = 0;
+      secondNumber = 1;
+    }
+    let FibonacciNumber = firstNumber + secondNumber;
+    FibonacciArray.push(FibonacciNumber)
+    console.log(FibonacciNumber);
+  };
+  console.log(FibonacciArray)
+  return FibonacciArray
 };
 
+fibonacci(1);
+
 // Do not edit below this line
-module.exports = fibonacci;
+// module.exports = fibonacci;
