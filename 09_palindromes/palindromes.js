4a351525bbd5980b702767fb90088d3e634cf13c
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-09 00:40:07 -0500

    build ex. 9 basic palindrome test 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  //make an array of the word:
+  let wordArray = string.toLowerCase.split("")
 
+  //form a reversed copy of the array:
+  let reversedWordArray = wordArray.toReversed();
+
+    //simple conditional checks for palindrome-ness:
+  if (wordArray.join('') === reversedWordArray.join('')) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
