1acc211d1bdb9c2355999119643365d71d88589e
Autumn Hicks <ajhicks1511@gmail.com>
2024-12-09 19:20:10 -0500

    complete ex. 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestPerson = people[0];
+  for (const person of people) {
+    if (personAge(person) >= personAge(oldestPerson)) {
+      oldestPerson = person;
+    }
+  };
+  return oldestPerson;
 };
+
+const personAge = function(person) {
+  const yearOfDeath = person.yearOfDeath || new Date().getFullYear();
+  return yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
